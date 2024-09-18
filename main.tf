7b535a3939ef4a5960e37e7ae1224273aade2f8e
techtorialdevops <130173463+techtorialdevops@users.noreply.github.com>
2024-09-17 20:44:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,9 +72,9 @@ module "eks" {
 
       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 3
+      max_size     = 5
+      desired_size = 5
     }
 
     two = {
