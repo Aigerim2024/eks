a76819bae5c5340c9088c519dbc94ff1e5bfcd8f
Aigerim2024 <llc.sfdev@gmail.com>
2024-10-01 08:25:41 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "techtorialeks"
+    bucket = "aws-backet-b"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
